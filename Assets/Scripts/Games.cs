96e31752b358dfd4921d11a8fb3356a433c6e6a7
Hankyyh <yunhany@hotmail.com>
2017-06-04 17:38:00 -0700

    player movement

---
+++ Assets/Scripts/Games.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Games : MonoBehaviour {
+	public Player player1;
+	public Color player1Color = Color.green;
+
+	// Use this for initialization
+	void Start () {
+		player1.canMove = true;
+		player1.GetComponent<SpriteRenderer> ().color = player1Color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
