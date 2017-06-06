ff9f8d3e592ebb6f95f8dca33f217eff0a44c04f
test <cao896@usc.edu>
2017-06-06 13:31:18 -0700

    stage1
    controler finish

---
+++ ready/Assets/floatcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class floatcontroller : MonoBehaviour {
+	Rigidbody2D mybody;
+	public float moveforce = 50,bootmultiper=2;
+	// Use this for initialization
+	void Start () {
+		mybody = this.GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical"))*moveforce;
+		bool isBoosting = CrossPlatformInputManager.GetButton ("Jump");
+		Debug.Log (isBoosting ? bootmultiper:1);
+		mybody.AddForce (movevec*(isBoosting?bootmultiper:1));
+	}
+}
---
+++ shoot/Assets/playershooter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playershooter : MonoBehaviour {
+	public float shootRate = 2;  
+	public float attack = 30;  
+	private float timer = 0;  
+	private ParticleSystem particleSystem;  
+	private LineRenderer lineRenderer; 
+
+
+
+	// Use this for initialization
+	void Start () {
+		particleSystem = this.GetComponentInChildren<ParticleSystem>();  
+		lineRenderer = this.GetComponent<Renderer>() as LineRenderer;
+	}
+
+	void OnEnable()
+	{
+		EasyButton.On_ButtonPress += On_ButtonPress;  
+
+	}
+
+
+	void On_ButtonPress(string buttonName)  
+	{     
+		//如果按下的是fire Button  
+		if (buttonName == "Fire")  
+		{  
+			timer += Time.deltaTime;  
+			if (timer > 1 / shootRate)  
+			{  
+				timer -= 1 / shootRate;  
+				Shoot();  
+			}  
+		}  
+	}  
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {}
+
+	void Shoot() {  
+		GetComponent<Light>().enabled = true;  
+		particleSystem.Play();  
+		this.lineRenderer.enabled = true;  
+		lineRenderer.SetPosition(0, transform.position);  
+		Ray ray = new Ray(transform.position, transform.forward);  
+		RaycastHit hitInfo;  
+		if (Physics.Raycast(ray, out hitInfo))
+		{  
+			lineRenderer.SetPosition(1, hitInfo.point);  
+			//判断当前的射击有没有碰撞到敌人  
+			if (hitInfo.collider.tag == Tags.enemy) {  
+				hitInfo.collider.GetComponent<EnemyHealth>().TakeDamage(attack,hitInfo.point);  
+			}  
+
+		} else {  
+			lineRenderer.SetPosition(1, transform.position + transform.forward * 100);  
+		}  
+		//播放射击音效  
+		GetComponent<AudioSource>().Play();  
+
+		Invoke("ClearEffect", 0.05f);  
+	}  
+
+
+
+
+
+
+
+
+
+}
