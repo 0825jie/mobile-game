466d8d056b7d7bd3c4f3a75c040b545a97ee7878
0825jie <876999351@qq.com>
2017-06-08 14:46:52 -0700

    newend
    new end

---
+++ Bell-project/Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//c
+using System;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject playerPrefab;
+//	text
+	public Text continueText;
+	public Text scoreText;
+
+	private float timeElapsed = 0f;
+	private float bestTime = 0f;
+	private float blinkTime = 0f;
+	private bool blink;
+	private bool gameStarted;
+	private TimeManager timeManager;
+	private GameObject player;
+	private GameObject floor;
+	private Spawner spawner;
+
+	void Awake(){
+		floor = GameObject.Find ("Foreground");
+		spawner = GameObject.Find ("Spawner").GetComponent<Spawner> ();
+		timeManager = GetComponent<TimeManager> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		var floorHeight = floor.transform.localScale.y;
+
+		var pos = floor.transform.position;
+		pos.x = 0;
+		pos.y = -((Screen.height / PixelPerfectCamera.pixelsToUnits) / 2) + (floorHeight / 2);
+		floor.transform.position = pos;
+
+		spawner.active = false;
+
+		Time.timeScale = 0;
+
+		continueText.text = "Kill Bruins&Find Bell,press to start";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameStarted && Time.timeScale == 0) {
+
+			if(Input.anyKeyDown){
+
+				timeManager.ManipulateTime(1, 1f);
+				ResetGame();
+			}
+		}
+
+//		text
+
+		if (!gameStarted) {
+			blinkTime ++;
+
+			if (blinkTime % 40 == 0) {
+				blink = !blink;
+			}
+
+			continueText.canvasRenderer.SetAlpha (blink ? 0 : 1);
+
+			scoreText.text = "TIME: " + FormatTime (timeElapsed) + "\n MASTER KILLED: XX" +"\n LEVEL: 1";
+		} else {
+			timeElapsed += Time.deltaTime;
+			scoreText.text = "TIME: " + FormatTime (timeElapsed) + "\n MASTER KILLED: XX" +"\n LEVEL: 1";
+		}
+	}
+
+	void OnPlayerKilled(){
+		spawner.active = false;
+
+		var playerDestroyScript = player.GetComponent<DestroyOffscreen> ();
+		playerDestroyScript.DestroyCallback -= OnPlayerKilled;
+
+		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		timeManager.ManipulateTime (0, 5.5f);
+		gameStarted = false;
+
+		continueText.text = "Kill Bruins&Find Bell,press to start";
+	}
+
+	void ResetGame(){
+		spawner.active = true;
+
+		player = GameObjectUtil.Instantiate(playerPrefab, new Vector3(0, (Screen.height/PixelPerfectCamera.pixelsToUnits)/2 + 100, 0));
+
+		var playerDestroyScript = player.GetComponent<DestroyOffscreen> ();
+		playerDestroyScript.DestroyCallback += OnPlayerKilled;
+
+		gameStarted = true;
+
+		continueText.canvasRenderer.SetAlpha(0);
+
+		timeElapsed = 0;
+	}
+
+	string FormatTime(float value){
+		TimeSpan t = TimeSpan.FromSeconds (value);
+
+		return string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
+	}
+
+}
