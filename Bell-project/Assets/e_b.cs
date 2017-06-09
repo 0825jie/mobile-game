ce7b5a2e69859d37aff71d1c611f218c773a6994
yli909 <yli909@usc.edu>
2017-06-08 18:37:15 -0700

    save before merge

--- Bell-project/Assets/e_b.cs
+++ Bell-project/Assets/e_b.cs
@@ -14,12 +14,13 @@ public class e_b : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-//		if (other.transform.tag == "player") {
-//			Destroy(other.gameObject);
-//			gameObject.SetActive(false);
-//		}
-//		if (other.transform.tag == "map") {
-//			gameObject.SetActive (false);
-//		}
+
+		if (other.transform.tag == "player") {
+			Destroy(other.gameObject);
+			gameObject.SetActive(false);
+		}
+		if (other.transform.tag == "zidanqiang") {
+			gameObject.SetActive (false);
+		}
 	}
 }
