2a5a6664baa3a960b365e4e4160931acb962248a
yli909 <yli909@usc.edu>
2017-06-20 20:48:24 -0700

    子弹，血量

--- Bell-project/Assets/enemy_bullet_collision.cs
+++ Bell-project/Assets/enemy_bullet_collision.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class enemy_bullet_collision : MonoBehaviour
 {
 
+	float lifetime = 10;
+	int time=0;
 	// Use this for initialization
 	void Start ()
 	{
@@ -14,13 +16,15 @@ public class enemy_bullet_collision : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		time += 1;
+		if (time >= lifetime) {
+			Destroy (gameObject);
+		}
 	}
 
 
 	private void OnCollisionEnter (Collision collision)
 	{ 
-		        
 		if (collision.transform.tag != "e_bullet" && collision.transform.tag != "enemy") {
 
 			gameObject.SetActive (false);
